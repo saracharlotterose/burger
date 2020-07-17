cb0aaf4800e8e6a1ad2891ed512c55dc453cd90b
Sara Webb <saracharlotterose@gmail.com>
2020-07-17 14:44:23 -0700

    saving schema changes

---
+++ config/orm.js
@@ -0,0 +1,56 @@
+// Import MySQL connection.
+const connection = require("../config/connection.js");
+
+// Object for all our SQL statement functions.
+const orm = {
+  all: (tableInput, cb) => {
+    const queryString = "SELECT * FROM ??";
+    connection.query(queryString, [tableInput], (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  create: (table, newRowData, cb) => {
+    const queryString = "INSERT INTO ?? SET ?";
+    const values = [table, newRowData];
+
+    connection.query(queryString, values, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  // Example of updateValues: { name: "panther", sleepy: true }
+  // Example of condition: { id: 1 }
+  update: (table, updateValues, condition, cb) => {
+    const queryString = "UPDATE ?? SET ? WHERE ?";
+    const values = [table, updateValues, condition];
+
+    console.log(queryString);
+    connection.query(queryString, values, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // Delete row(s) from table with given condition.
+  // Example condition: { id: 1 }
+  delete: (table, condition, cb) => {
+    const queryString = "DELETE FROM ?? WHERE ?";
+    const values = [table, condition];
+    connection.query(queryString, values, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
+
+// Export the orm object
+module.exports = orm;
